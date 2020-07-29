cced504895a8b7bd83ebc01c0cb4f0d23c712755
Derek Stampone <derekstampone@gmail>
2020-07-29 00:53:11 +0000

    first two redirects

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
-# ---- YOUR APP STARTS HERE ----
-# -- Import section --
 from flask import Flask
-from flask import render_template
-from flask import request
-from flask import session
+from flask import render_template, redirect, url_for
 
-import requests #To access our API
 
 # -- Initialization section --
 app = Flask(__name__)
-
-## secret key for session (In production, you would set this key via an environment variable)
-app.secret_key = b'HO\xf8\xff+\n\x1e\\~/;}'
 
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    data = {}
+    redirects_list = [
+        {'function':'redirect_one', 
+        'description':"redirect('/index')",
+        'color':'bg-danger'},
+        {'function':'redirect_two', 
+        'description':"redirect(f'''http://0.0.0.0:5000{url_for('index')}''')",
+        'color':'bg-success'},
+    ]
+    data['redirects_list']=redirects_list
+    return render_template('index.html', data=data)
+
+@app.route('/redirect/one')
+def redirect_one():
+    return redirect('/index')
+
+@app.route('/redirect/two')
+def redirect_two():
+    return redirect(f"http://0.0.0.0:5000{url_for('index')}")
